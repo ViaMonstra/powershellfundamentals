ce27b9b46ff69ac7c2ca99f886be0ce5c46438dd
arwidmark <arwidmark@gmail.com>
2023-05-16 12:40:32 -0500

    Create Get Reliability Records.ps1

---
+++ Module 1/Get Reliability Records.ps1
@@ -0,0 +1,5 @@
+﻿# Get Reliability Records
+Get-CimInstance -ClassName Win32_ReliabilityRecords -property Message | select-object -first 5 Message | format-list *
+ 
+ # Find Restore Points with PowerShell
+Get-EventLog -Logname Application -InstanceId 8194 -Newest 3
