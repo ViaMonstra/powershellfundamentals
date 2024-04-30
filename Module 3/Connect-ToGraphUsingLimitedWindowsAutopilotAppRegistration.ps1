8a5dc4a777edc510f64ba242633aa295af5222dc
arwidmark <arwidmark@gmail.com>
2024-04-30 10:38:05 -0500

    Create Connect-ToGraphUsingLimitedWindowsAutopilotAppRegistration.ps1

---
+++ Module 3/Connect-ToGraphUsingLimitedWindowsAutopilotAppRegistration.ps1
@@ -0,0 +1,16 @@
+﻿# Set Authentication info for to Microsoft Graph
+$tenant = "replace-with-your-tenant-name"
+$authority = "https://login.windows.net/$tenant"
+$AppID = "replace-with-your-app-registration-id"
+$AppSecret = Get-Content "C:\Setup\Credentials\secret.txt"
+
+# Show the settings
+Write-Host "Intune tenant is $tenant"
+Write-Host "Authority tenant is $authority"
+Write-Host "AppID is $AppID"
+Write-Host "AppSecret is **Secret**"
+
+# Connect to Microsoft Graph
+Update-MSGraphEnvironment -AppId $AppID -Quiet
+Update-MSGraphEnvironment -AuthUrl $authority -Quiet
+Connect-MSGraph -ClientSecret $AppSecret -Quiet
